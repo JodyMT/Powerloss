6555f75de04ce1948a09e6c68d202356da729634
JodyMT <42037484+JodyMT@users.noreply.github.com>
2018-11-13 13:01:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "jody"
 plugin_author_email = "jmt.theunissen@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://testhomepage"
+plugin_url = "https://github.com/JodyMT/Powerloss"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
